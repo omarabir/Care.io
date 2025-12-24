92074f313d52a5bf0400a1c2a58176061e47057a
omarabir <omarabir@gmail.com>
2025-12-24 13:14:26 +0600

    Update layout.js, Add SessionProvider.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,9 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
+
 import Footer from "@/components/Footer";
 import SessionProviderWrapper from "@/components/SessionProviderWrapper";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
